bc05980385d3e30c737eb52799b87e2ea772e614
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-21 22:41:51 +0600

    Dynamically display loaded data on website

--- js/api.js
+++ js/api.js
@@ -17,5 +17,10 @@ function loadPosts() {
 }
 
 function displayUsers(data) {
-    console.log(data);
+    const ul = document.getElementById('users');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = `name: ${user.name} email: ${user.email}`;
+        ul.appendChild(li);
+    }
 }
